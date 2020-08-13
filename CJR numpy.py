8e4840a41ffc30e1e7d0c66e415a78de06a5d2e3
coreyjr2 <56316889+coreyjr2@users.noreply.github.com>
2020-08-13 12:30:36 -0500

    Update CJR numpy.py

--- CJR numpy.py
+++ CJR numpy.py
@@ -26,6 +26,7 @@ arrays'''
 #Let's make some arrays
 
 import numpy as np
+import matplotlib as plt
 
 my_list = ([1,2,3],[4,5,6],[7,8,9])
 my_array = np.array(my_list)
@@ -43,31 +44,62 @@ print(random_array)
 print("The shape of this array is:", array_3d.shape)
 
 
+
 #Let's do some matrix operations on the arrays
 a = np.array([1,2,3,4,5])
 b = np.array([3,4,5,8,1])
 
+
+#subtract the values of a from b
 a-b
 
+#Let's take the natural log of each element of array a
+np.log(a)
+
+###Let's do some basic linear algebra###
+x = np.random.normal(0,1, size =(4,4,4))
+np.linalg.inv(x)
+
+#Let's make an identity matrix (of dimensions 5x5)
+np.eye(5)
+
+#Let's find the inverse of a matrix
+np.linalg.inv()
+
+
+## some other stuff related to array broadcasting
+arr_2d = np.zeros([5,5])
+arr_ones = np.ones([3,3])
+arr_2d[:3,2:] = arr_ones
+arr_2d
+
+'''What we did here was nest the array of ones (3x3) inside a 
+another array of zeros with dimensions 5x5'''
 
 
 
 
 
 
+#Let's do some more indexing with arrays
 
+#make a 4x4 array
+x = np.reshape(np.arange(16), (4,4))
+print("Let's index this array:", x)
 
-
-
-
-
-
-
-
+i = np.array([0,3])
+j = np.array([1,1])
 
 
 
 ##Now let's simulate some brain imaging data
+
+'''#Now, this isnt *really* like what you would have in an image, because it is lacking
+#information from the time dimension, and we have values in every voxel, as 
+#opposed to only where there would be actual brain tissue, but it will suffice 
+#for now
+
+
 random_brain = np.random.normal(0,1, size =(100,100,100))
 print("The dimensions of voxels in this simulated brain are:", random_brain.shape)
 
@@ -77,6 +109,46 @@ print("The value of the voxel at dimensions 10,24,50 is equal to :", random_brai
 #Let's take a slice out of the three dimensional image
 print(random_brain[:,:,3])
 
+
+### Let's do some data filtering on the basis of boolean values
+is_big = random_brain > 2
+big_values = random_brain[is_big]
+big_values.shape
+#So it looks like 22676 values are over 2 standard deviations above the mean
+
+#Copy the original image to prevent modification
+filtered_brain = random_brain.copy()
+
+#set all voxel values greater than 2sd to 0
+filtered_brain[~is_big] = 0
+filtered_brain.shape
+# the tilde ~ means "not" in this case
+
+##let's do some plotting
+import matplotlib.pyplot as plt
+
+#take a slice of each
+orig_slice = random_brain[:, :, 45]
+filter_slice = filtered_brain[:, :, 45]
+
+
+#Now let's compare original and thresholded image
+fig, axes = plt.subplots(1, 2, figsize=(4,10))
+axes[0].imshow(orig_slice)
+axes[1].imshow(filter_slice)
+
+#Some tips on vectorization
+#looping over some values
+x = np.arange(5)
+y = np.zeros((5,))
+for i in range (5):
+    y[i] = x[i] + 1
+y
+
+#or you could do this
+x + 1
+#The latter way is much faster
+#Try to vectorize your code always!!
 
 
 
